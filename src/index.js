873d7dd82834202804110407d9ae91705eaa5e70
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2021-07-14 19:49:01 +0300

    make functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.max = function max(numbers = 0) {
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (result < numbers[i]) {
+            result = numbers[i];
+        }
+    }
+    if (numbers != 0) {
+        return result;
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.min = function min(numbers = 0) {
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (result > numbers[i]) {
+            result = numbers[i]
+        }
+    }
+    if (numbers != 0) {
+        return result;
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(numbers = 0) {
+    let result = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        result += numbers[i];
+    }
+    if (numbers != 0) {
+        return result / numbers.length;
+    } else {
+        return 0;
+    }
 }
